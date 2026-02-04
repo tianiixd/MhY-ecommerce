48a0a5513c85b65fcd5ce53bcc0f62995c3823a3
TianiiiXD <tian90921@gmail.com>
2026-02-04 14:01:01 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col w-full min-h-dvh bg-neutral-100 py-10">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold -tracking-tight bg-clip-text bg-linear-to-tr from-red-500 via-purple-500 to-blue-500  text-transparent text-center">
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
